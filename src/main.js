e1d8199dc20f205394e63ac2ce70bdd427c17a97
Anastasiia-Stefantsova <152818875+Anastasiia-Stefantsova@users.noreply.github.com>
2024-05-23 00:29:00 +0300

    HomeWork11-3

--- src/main.js
+++ src/main.js
@@ -1,43 +1,44 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import { createMarkup } from './js/render-functions';
-import { fetchPhotos } from './js/pixabay-api';
 
-const imgContainer = document.querySelector('.gallery');
-const searchForm = document.querySelector('.search-form');
-const loaderEl = document.querySelector('.loader');
+import { fetchPhotos } from './js/pixabay-api';
+import { createMarkup } from './js/render-functions';
+
+const galleryEl = document.querySelector('.js-gallery');
+const searchFormEl = document.querySelector('.js-search-form');
+const loaderEl = document.querySelector('.js-loader');
 
 function onSearch(event) {
   event.preventDefault();
-  
-  const searchQuery = event.target.elements.searchKeyword.value.trim();
-  imgContainer.innerHTML = '';
-  if (searchQuery === '') {
+  const query = event.target.elements.searchword.value.trim();
+  galleryEl.innerHTML = '';
+  if (query === '') {
     return iziToast.error({
       message:
         'Sorry, there are no images matching your search query. Please try again!',
+      position: 'topRight',
     });
   }
-  imgContainer.innerHTML = '';
   loaderEl.classList.remove('is-hidden');
 
-  fetchPhotos(searchQuery)
+  fetchPhotos(query)
     .then(imagesData => {
       if (imagesData.hits.length === 0) {
         iziToast.error({
           message:
             'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
         });
       }
-      
-      imgContainer.innerHTML = createMarkup(imagesData.hits);
-      const lightbox = new SimpleLightbox('.gallery a', {
+
+      galleryEl.innerHTML = createMarkup(imagesData.hits);
+      const lightbox = new SimpleLightbox('.item-list-link', {
         captionsData: 'alt',
         captionsDelay: 250,
       });
-    
     })
     .catch(error => console.log(error))
     .finally(() => {
@@ -45,4 +46,8 @@ function onSearch(event) {
       loaderEl.classList.add('is-hidden');
     });
 }
-searchForm.addEventListener('submit', onSearch);
+
+searchFormEl.addEventListener('submit', onSearch);
+
+
+
